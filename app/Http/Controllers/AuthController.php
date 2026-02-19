d0f9e0bcb750f49d827e4b9545b17b4db7df8d98
Angelaaa-hub <angela.mureithi@strathmore.edu>
2026-02-19 12:29:42 +0300

    second commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,5 +17,27 @@ class AuthController extends Controller
     $user->name = $validated['name'];
     $user->email = $validated['email'];
 
+    $password = Hash::make($validated['password']);
+
+    try{
+        $user->save();
+        return response()->json($user);
+    
     }
+
+    catch(\Exception $exception){
+        return response->jsom([
+            'error'=>"Registration Failed!",
+            'message'=>$exception->getMessage()
+        ]);
+    }
+    }
+
+    public function login(Request $request){
+    $validated = $request->validate([
+    'email'=>'required|email|unique:users,email',
+    'password'=>'required|string|min:4'
+    ]);
+    }
+
 }
